bd3abee07e91262903d0e1a930fd42072429db73
Sherry Nyeo <snyeo2630@gmail.com>
2021-01-16 20:21:30 -0700

    added comments

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -71,13 +71,13 @@ class Skeleton extends Component {
         </ul>
         <h2> What you need to change</h2>
         <ul>
-          <li>Change the font in utilities.css</li>
-          <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
-          <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
+          DONE: <li>Change the font in utilities.css</li>
+          DONE: <li>Change the Frontend CLIENT_ID for Google Auth (Skeleton.js)</li>
+          DONE: <li>Change the Server CLIENT_ID for Google Auth (auth.js)</li>
           <li>Change the Database SRV for Atlas (server.js)</li>
           <li>Change the Database Name for MongoDB (server.js)</li>
-          <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
-          <li>Update website title in client/dist/index.html</li>
+          DONE: <li>Add a favicon to your website at the path client/dist/favicon.ico</li>
+          DONE: <li>Update website title in client/dist/index.html</li>
         </ul> */}
       </>
     );
